bde842bf9e341382fc225669fd19a46248a0e344
PauGuiot15 <pguiot15@elpuig.xeill.net>
2024-04-21 16:39:07 +0200

    testdos

--- MP05UF2P2/src/ex2/HashTable.java
+++ MP05UF2P2/src/ex2/HashTable.java
@@ -4,10 +4,7 @@ package ex2;
 // Modified by Fernando Porrino Serrano for academic purposes.
 
 import java.util.ArrayList;
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 /**
  * Implementació d'una taula de hash sense col·lisions.
@@ -229,35 +226,6 @@ public class HashTable {
         log("\nValue for key(20) : " + hashTable.get("20") );
     }
     
-
-
-    @Test
-    public void testPutAndGet() {
-        HashTable hashTable = new HashTable();
-        hashTable.put("key1", "value1");
-        hashTable.put("key2", "value2");
-        assertEquals("value1", hashTable.get("key1"));
-        assertEquals("value2", hashTable.get("key2"));
-    }
-
-    @Test
-    public void testDrop() {
-        HashTable hashTable = new HashTable();
-        hashTable.put("key1", "value1");
-        hashTable.drop("key1");
-        assertNull(hashTable.get("key1"));
-    }
-
-    @Test
-    public void testCountAndSize() {
-        HashTable hashTable = new HashTable();
-        assertEquals(0, hashTable.count());
-        assertEquals(16, hashTable.size());
-        hashTable.put("key1", "value1");
-        assertEquals(1, hashTable.count());
-        hashTable.put("key2", "value2");
-        assertEquals(2, hashTable.count());
-    }
 
 
     private static void log(String msg) {
---
+++ MP05UF2P2/src/ex2/HashTableTest.java
@@ -0,0 +1,38 @@
+package ex2;
+
+import ex2.HashTable;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class HashTableTest {
+
+    @Test
+    public void testPutAndGet() {
+        HashTable hashTable = new HashTable();
+        hashTable.put("key1", "value1");
+        hashTable.put("key2", "value2");
+        assertEquals("value1", hashTable.get("key1"));
+        assertEquals("value2", hashTable.get("key2"));
+    }
+
+    @Test
+    public void testDrop() {
+        HashTable hashTable = new HashTable();
+        hashTable.put("key1", "value1");
+        hashTable.drop("key1");
+        assertNull(hashTable.get("key1"));
+    }
+
+    @Test
+    public void testCountAndSize() {
+        HashTable hashTable = new HashTable();
+        assertEquals(0, hashTable.count());
+        assertEquals(16, hashTable.size());
+        hashTable.put("key1", "value1");
+        assertEquals(1, hashTable.count());
+        hashTable.put("key2", "value2");
+        assertEquals(2, hashTable.count());
+    }
+}
